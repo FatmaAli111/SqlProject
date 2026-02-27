e17c716f0c78d58dd4b58060509aaf889c07be72
Fatma_Ali <fa5675869@gmail.com>
2026-02-27 22:03:00 +0200

    Create Tables : Exam,ExamAllowanceOptions,Instructor,QuestionChoices

--- TableCreationScripts/FatmaScript.sql
+++ TableCreationScripts/FatmaScript.sql
@@ -1,32 +1,65 @@
 create table Exam(
-ExamID int primary Key identity,
+ExamID int primary Key identity(1,1),
 [Name] varchar(20) not null,
 [Type] varchar(10) not null check([Type] in ('Corrective','Exam')),
-StartDate Date not null,
-EndDate Date check(EndDate >StartDate) not null,
---allowance options
-CourseID int foreign key (CourseID) references Course(CourseID),
-InstructorID int foreign key (InstructorID) references Instructor(InstructorID)
+StartTime time not null,
+EndTime time  not null,
+[Day] date ,
+CourseID int,
+InstructorID int,
+foreign key (CourseID) references Course(CourseID),
+foreign key (InstructorID) references Instructor(InstructorID),
+constraint chkExamTime check(EndTime >StartTime),
+constraint chkExamTotalTime check(DateDiff(minute, StartTime, EndTime) >=60)
+
+)
+create table ExamAllowanceOptions (
+OptionText varchar(20),
+ExamID int foreign key (ExamID) references Exam(ExamID),
+primary key(OptionText,ExamID)
 )
 --constraint check if time between end and start > hour
-create table QuestionPool(
-QuestionID int primary Key identity,
-QuestionText nvarchar(max),
-[Type] varchar(4) check([Type] in ('Text','MCQ','T&F'))
-CorrectAnswer varchar(max),
-CourseID int foreign key (CourseID) references Course(CourseID) not null
-)
+--create table QuestionPool(
+--QuestionID int primary Key identity,
+--QuestionText nvarchar(max),
+--[Type] varchar(4) check([Type] in ('Text','MCQ','T&F'))
+--CorrectAnswer varchar(max),
+--CourseID int not null,
+--foreign key (CourseID) references Course(CourseID) 
+--)
 
 create table Contain(
 Degree int not null,
 StudentAnswer varchar(max) ,
-ExamID int foreign key (ExamID) references Exam(ExamID) not null,
-QuestionID int foreign key (QuestionID) references QuestionPool(QuestionID) not null,
+QuestionID int not null,
+ExamID int not null,
+foreign key (ExamID) references Exam(ExamID) ,
+foreign key (QuestionID) references QuestionPool(QuestionID) ,
 primary key (ExamID,QuestionID)
 )
 
 create table QuestionChoices(
-ChoicesID int primary key identity,
-ChoiceText varchar(50) ,
-QuestionID int foreign key (QuestionID) references QuestionPool(QuestionID) 
+ChoicesID int primary key identity(1,1),
+ChoiceText varchar(50) unique,
+QuestionID int,
+foreign key (QuestionID) references QuestionPool(QuestionID) 
+)
+
+create table Instructor(
+InstructorID int primary key identity(1,1),
+FullName varchar(20) not null ,
+Email nvarchar(20) unique,
+CourseID int,
+ManagerID int,
+foreign key (ManagerID) references Instructor(InstructorID),
+foreign key (CourseID) references Course(CourseID)
+
+)
+create type PhoneUDD from varchar(11) not null 
+create  table InstructorPhone(
+phone  PhoneUDD CHECK (phone LIKE '01[0-9]%'),
+InstructorID int,
+
+foreign key (InstructorID) references Instructor(InstructorID),
+primary key(phone,InstructorID)
 )
